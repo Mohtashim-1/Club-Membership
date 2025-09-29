b8391b5c15e9d52cc429dc0f1f5cc332656891af
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-09-29 13:05:10 +0500

    chore: fix in doctype

--- club_membership/club_membership/doctype/member/member.py
+++ club_membership/club_membership/doctype/member/member.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Member(Document):
-	pass
+	def before_validate(self):
+		# ensure member_id is set before mandatory validation
+		if not getattr(self, 'member_id', None):
+			self.member_id = self.name
+
+	def before_insert(self):
+		# ensure member_id follows the autoname (MEM series)
+		self.member_id = self.name
